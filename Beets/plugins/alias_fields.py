1ab43ff768ae0c72b2fa9e144375e0b2b02baf98
Christopher Larson <kergoth@gmail.com>
2019-05-21 15:01:20 -0700

    Add alias_fields plugin
    This lets you define additional fields in the form of format strings,
    which is particularly useful to reduce duplication in path formats,
    since I can alias common fragments. I should probably rename it.
    
    ```yaml
    alias_fields:
      album_fields:
        testfield: '$albumartist - $album'
    
      item_fields:
        singletrack_path: 'Single Tracks/$artist - $title'
    ```
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ Beets/plugins/alias_fields.py
@@ -0,0 +1,31 @@
+from __future__ import division, absolute_import, print_function
+
+from beets.plugins import BeetsPlugin
+
+
+class AliasFieldsPlugin(BeetsPlugin):
+    """Add aliases / saved fields for use in format strings.
+
+    Similar to inline, except we specify a format string, not code.
+    """
+
+    def __init__(self):
+        super(AliasFieldsPlugin, self).__init__()
+
+        self.config.add({
+            'item_fields': {},
+            'album_fields': {},
+        })
+
+        for field, template in self.config['item_fields'].items():
+            def apply(i, field=field, template=template):
+                return self.apply_template(field, template.get(str), i, False)
+            self.template_fields[field] = apply
+
+        for field, template in self.config['album_fields'].items():
+            def apply(i, field=field, template=template):
+                return self.apply_template(field, template.get(str), i, True)
+            self.album_template_fields[field] = apply
+
+    def apply_template(self, field, template, model, is_album):
+        return model.evaluate_template(template)
