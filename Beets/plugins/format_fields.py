91f0e6823facd9a8235e03803cd709e24203d027
Christopher Larson <kergoth@gmail.com>
2019-05-22 15:14:57 -0700

    Rename alias_fields to format_fields
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- Beets/plugins/format_fields.py
+++ Beets/plugins/format_fields.py
@@ -3,14 +3,14 @@ from __future__ import division, absolute_import, print_function
 from beets.plugins import BeetsPlugin
 
 
-class AliasFieldsPlugin(BeetsPlugin):
-    """Add aliases / saved fields for use in format strings.
+class FormatFieldsPlugin(BeetsPlugin):
+    """Define new fields using format strings.
 
     Similar to inline, except we specify a format string, not code.
     """
 
     def __init__(self):
-        super(AliasFieldsPlugin, self).__init__()
+        super(FormatFieldsPlugin, self).__init__()
 
         self.config.add({
             'item_fields': {},
