47fa68528d718c79fc79aa99375a5434a6d94d60
Christopher Larson <kergoth@gmail.com>
2019-06-06 09:29:53 -0700

    alias_query: fix so we can use 'clause' to speed it up
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- Beets/plugins/alias_query.py
+++ Beets/plugins/alias_query.py
@@ -1,8 +1,6 @@
 from __future__ import division, absolute_import, print_function
 
-import confuse
-
-from beets import config, ui
+from beets import config
 from beets.library import parse_query_string, Item, Album
 from beets.plugins import BeetsPlugin
 
@@ -10,52 +8,48 @@ from beets.dbcore import Query
 
 
 class AliasQuery(Query):
+    name = None
+    config = config['alias_query']
+
     def __init__(self, alias):
         self.alias = alias
+        config = self.config[self.name + '_queries']
+        self.query_string = config[alias].as_str()
+        self.query, _ = parse_query_string(self.query_string, self.model)
+
+    def clause(self):
+        return self.query.clause()
 
     def match(self, item):
-        if isinstance(item, Item):
-            try:
-                query_string = self.item_aliases[self.alias]
-            except KeyError:
-                raise confuse.ConfigError(u'alias.item_queries.%s not found' % self.alias)
-        elif isinstance(item, Album):
-            try:
-                query_string = self.album_aliases[self.alias]
-            except KeyError:
-                raise confuse.ConfigError(u'alias.album_queries.%s not found' % self.alias)
-        else:
-            raise ui.UserError('unable to handle non-Item/Album Models')
-
-        query, parsed_sort = parse_query_string(query_string, item.__class__)
-        return query.match(item)
+        return self.query.match(item)
 
 
-class AliasQueryPlugin(BeetsPlugin):
+class ItemAliasQuery(AliasQuery):
+    name = 'item'
+    model = Item
+
+
+class AlbumAliasQuery(AliasQuery):
+    name = 'album'
+    model = Album
+
+
+class AliasqueryPlugin(BeetsPlugin):
     """Aliased queries support.
 
     Allow definition of named / aliased queries.
     """
 
     def __init__(self):
-        super(AliasQueryPlugin, self).__init__()
+        super(AliasqueryPlugin, self).__init__()
 
         self.config.add({
             'item_queries': {},
             'album_queries': {},
         })
 
-        self.item_aliases, self.album_aliases = {}, {}
-        for name in self.config['item_queries'].keys():
-            query = self.config['item_queries'][name]
-            self.item_aliases[name] = query.as_str()
-        for name in self.config['album_queries'].keys():
-            query = self.config['album_queries'][name]
-            self.album_aliases[name] = query.as_str()
+        if self.config['item_queries']:
+            self.item_queries = {'alias': ItemAliasQuery}
 
-        class ConfiguredAliasQuery(AliasQuery):
-            item_aliases = self.item_aliases
-            album_aliases = self.album_aliases
-
-        self.item_queries = {'alias': ConfiguredAliasQuery}
-        self.album_queries = {'alias': ConfiguredAliasQuery}
+        if self.config['album_queries']:
+            self.album_queries = {'alias': AlbumAliasQuery}
