acb2a2c1232b5def699a5eebbc4bd6816dab16b2
Christopher Larson <kergoth@gmail.com>
2019-08-28 11:17:08 -0700

    replace: split replace from replace_path
    The latter operates per-path-component, the former on the whole. The
    former is useful when operating on a single field, the latter on a full
    path.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- Beets/plugins/replace_format.py
+++ Beets/plugins/replace_format.py
@@ -4,13 +4,22 @@ Path format function to apply the specified set of replacements to a path.
 This can be used to apply replacements only to the alternative library created
 by beets-alternatives, for example, while leaving the original library alone.
 
+`replace_path` operates on a per-path-component basis, while `replace`
+operates on the whole. The former is useful for replacing across a full path,
+the latter on a field, as the latter can handle the case where the field
+contains a path separator.
+
 Usage:
 
   foo_replace:
     Foo: Bar
 
+  bar_replace:
+    '/.*': ''
+
   paths:
-    default: '%replace{foo_replace,$album/$track - $artist - $title}'
+    default: '%replace_path{foo_replace,$album/$track - $artist - $title}'
+    genre:foo: 'Foo/%replace{bar_replace,$album}/$track - $artist - $title'
 """
 
 from __future__ import division, absolute_import, print_function
@@ -28,11 +37,19 @@ class ReplacePlugin(BeetsPlugin):
     def __init__(self):
         super(ReplacePlugin, self).__init__()
         self.template_funcs['replace'] = _tmpl_replace
+        self.template_funcs['replace_path'] = _tmpl_replace_path
 
 
-def _tmpl_replace(config_path, path):
+def _tmpl_replace_path(config_path, path):
     replacements = get_replacements(config_path)
     return util.sanitize_path(path, replacements)
+
+
+def _tmpl_replace(config_path, string):
+    replacements = get_replacements(config_path)
+    for regex, repl in replacements:
+        string = regex.sub(repl, string)
+    return string
 
 
 # Copied with tweak from beets itself
