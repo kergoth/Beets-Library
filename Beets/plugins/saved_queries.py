55fd3a57990a0b46acb2324845d01ef681f0760d
Christopher Larson <kergoth@gmail.com>
2019-08-30 19:39:02 -0700

    Rename and update alias_query to saved_queries
    - Rename plugin from alias_query to saved_queries.
    - Move `item_queries` and `album_queries` from under the plugin config
      to toplevel to align with `item_fields` and `album_fields`.
    - Change the query names to `query` and `album_query` rather than
      `alias` and `album_alias` to clarify and align with the plugin name.
    - Add support for creation of fields that check the queries against the
      Item. I.e. both `query:is_christmas` and `is_christmas:1` will work,
      but the latter will be slow. More useful is to use it for inspection
      or path formats, i.e. `beet ls -f '$format_item $is_christmas'
      some_query`.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ Beets/plugins/saved_queries.py
@@ -0,0 +1,77 @@
+"""Support saved, named queries."""
+
+from __future__ import division, absolute_import, print_function
+
+from beets import config
+from beets.library import parse_query_string, Item, Album
+from beets.plugins import BeetsPlugin
+
+from beets.dbcore import types
+from beets.dbcore import Query
+
+
+class SavedQueries(Query):
+    model_name = None
+
+    def __init__(self, name):
+        self.name = name
+        queries = config[self.model_name + '_queries']
+        self.query_string = queries[name].as_str()
+        self.query, _ = parse_query_string(self.query_string, self.model)
+
+    def clause(self):
+        return self.query.clause()
+
+    def match(self, item):
+        return self.query.match(item)
+
+
+class ItemSavedQueries(SavedQueries):
+    model_name = 'item'
+    model = Item
+
+
+class AlbumSavedQueries(SavedQueries):
+    model_name = 'album'
+    model = Album
+
+
+class SavedQueriesPlugin(BeetsPlugin):
+    """Support saved, named queries.
+
+    Also support creation of Item/Album boolean fields for the query. Do not
+    query the fields, as the performance is not great. Use the `query:NAME`
+    query syntax for queries, and use the fields for inspection or a format
+    string.
+    """
+
+    def __init__(self):
+        super(SavedQueriesPlugin, self).__init__()
+
+        self.config.add({
+            'add_fields': True,
+        })
+
+        config.add({
+            'item_queries': {},
+            'album_queries': {},
+        })
+
+        self._log.debug('adding named item query `query`')
+        self._log.debug('adding named album query `album_query`')
+        self.item_queries = {'query': ItemSavedQueries}
+        self.album_queries = {'album_query': AlbumSavedQueries}
+        if self.config['add_fields'].get():
+            self.item_types = {}
+            self.template_fields = {}
+            for name in config['item_queries'].keys():
+                self._log.debug('adding item field {}', name)
+                self.item_types[name] = types.BOOLEAN
+                self.template_fields[name] = lambda item, name=name: ItemSavedQueries(name).match(item)
+
+            self.album_types = {}
+            self.album_template_fields = {}
+            for name in config['album_queries'].keys():
+                self._log.debug('adding album field {}', name)
+                self.album_types[name] = types.BOOLEAN
+                self.album_template_fields[name] = lambda album, name=name: AlbumSavedQueries(name).match(album)
