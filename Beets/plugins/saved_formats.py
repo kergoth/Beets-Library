e0edb2ec2edb65212ba881c5ed2c10e491a9a959
Christopher Larson <kergoth@gmail.com>
2019-08-31 21:35:24 -0700

    Rename format fields to saved formats and use toplevel configs
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- Beets/plugins/saved_formats.py
+++ Beets/plugins/saved_formats.py
@@ -1,37 +1,40 @@
+"""Define saved format strings by storing them in fields."""
+
 from __future__ import division, absolute_import, print_function
 
+from beets import config
 from beets.plugins import BeetsPlugin
 
 
-class FormatFieldsPlugin(BeetsPlugin):
-    """Define new fields using format strings.
+class SavedFormatsPlugin(BeetsPlugin):
+    """Define saved format strings by storing them in fields.
 
     Similar to inline, except we specify a format string, not code.
     """
 
     def __init__(self):
-        super(FormatFieldsPlugin, self).__init__()
+        super(SavedFormatsPlugin, self).__init__()
 
-        self.config.add({
-            'item_fields': {},
-            'album_fields': {},
+        config.add({
+            'item_formats': {},
+            'album_formats': {},
         })
-        self.set_template_fields()
+        self.set_template_formats()
 
-    def set_template_fields(self):
-        for field, template in self.config['item_fields'].items():
+    def set_template_formats(self):
+        for field, template in config['item_formats'].items():
             self._log.debug('adding item field {}', field)
 
             def apply(i, field=field, template=template):
                 return self.apply_template(field, template.as_str(), i)
-            self.template_fields[field] = apply
+            self.template_formats[field] = apply
 
-        for field, template in self.config['album_fields'].items():
+        for field, template in config['album_formats'].items():
             self._log.debug('adding album field {}', field)
 
             def apply(i, field=field, template=template):
                 return self.apply_template(field, template.as_str(), i)
-            self.album_template_fields[field] = apply
+            self.album_template_formats[field] = apply
 
     def apply_template(self, field, template, model):
         return model.evaluate_template(template)
