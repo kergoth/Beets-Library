fa04d1a67e6d0bf64887c68e5c9892350a03f97c
Christopher Larson <kergoth@gmail.com>
2019-09-06 18:30:13 -0700

    Add 'import_inspect' plugin
    This plugin's job is to show candidate/proposed metadata changes before
    they're applied. It adds a prompt choice when importing to inspect each
    candidate, and has an option (enabled by default) to show the changes to
    be applied after you hit Apply, and when timid, will prompt to continue.
    
    Note: if you choose not to continue after hitting apply, it will skip
    the current selection and move on to the next, rather than returning to
    the candidate menu, so at times it's better to use the menu option to
    proactively inspect, and only use the on_apply check as a final sanity
    check.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ Beets/plugins/import_inspect.py
@@ -0,0 +1,102 @@
+"""Inspect candidate changes when importing.
+
+Add a command to inspect candidate changes and has an option to automatically
+inspect the changes when the user selects Apply, and will prompt to continue
+when timid.
+"""
+
+from __future__ import division, absolute_import, print_function
+
+from beets import autotag, config, importer, library, ui
+from beets.plugins import BeetsPlugin
+from beets.ui.commands import PromptChoice
+
+
+def new_item(i):
+    ni = library.Item()
+    ni.update(i)
+    return ni
+
+
+class ImportInspectPlugin(BeetsPlugin):
+    nonalbum_fields = library.Item._media_tag_fields.difference(library.Album.item_keys)
+
+    def __init__(self):
+        super(ImportInspectPlugin, self).__init__()
+
+        self.config.add({
+            'on_apply': True,
+        })
+
+        self.register_listener('before_choose_candidate',
+                               self.before_choose_candidate_listener)
+        if self.config['on_apply'].get():
+            self.register_listener('import_task_choice',
+                                   self.import_task_choice_listener)
+
+    def before_choose_candidate_listener(self, session, task):
+        if task.candidates:
+            return [PromptChoice('n', 'iNspect changes',
+                                      self.importer_inspect_candidate)]
+
+    def importer_inspect_candidate(self, session, task):
+        # Prompt the user for a candidate.
+        sel = ui.input_options([], numrange=(1, len(task.candidates)))
+        match = task.candidates[sel - 1]
+
+        self.show_changes(session.lib, task, match)
+        return None
+
+    def import_task_choice_listener(self, session, task):
+        if task.apply:
+            if config['import']['quiet'].get():
+                return
+
+            changes = self.show_changes(session.lib, task)
+
+            # Only prompt when timid
+            if 'timid' in self.config:
+                timid = self.config['timid'].get(bool)
+            else:
+                timid = config['import']['timid'].get()
+
+            if not changes or not timid:
+                return
+
+            if not ui.input_yn(u'Continue to apply these changes (Y/n)?'):
+                task.set_choice(importer.action.SKIP)
+
+    def show_changes(self, lib, task, match=None):
+        if match is None:
+            match = task.match
+
+        changes = False
+        if task.is_album:
+            newmapping = {new_item(item): track_info for item, track_info in match.mapping.items()}
+            autotag.apply_metadata(match.info, newmapping)
+
+            # olditems[0].get_album() isn't working, create our own to compare
+            olditems = list(match.mapping.keys())
+            oldvalues = dict((key, olditems[0][key]) for key in library.Album.item_keys)
+            oldalbum = library.Album(lib, **oldvalues)
+
+            newitems = list(newmapping.keys())
+            values = dict((key, newitems[0][key]) for key in library.Album.item_keys)
+            album = library.Album(lib, **values)
+
+            album_changes = ui.show_model_changes(album, oldalbum, library.Album.item_keys)
+            if album_changes:
+                changes = True
+
+            new_by_info = {track_info: item for item, track_info in newmapping.items()}
+            for item, track_info in match.mapping.items():
+                newitem = new_by_info[track_info]
+                item_changes = ui.show_model_changes(newitem, item, self.nonalbum_fields)
+                if item_changes:
+                    changes = True
+        else:
+            fakeitem = new_item(task.item)
+            autotag.apply_item_metadata(fakeitem, match.info)
+            changes = ui.show_model_changes(fakeitem, task.item, library.Item._media_tag_fields)
+
+        return changes
