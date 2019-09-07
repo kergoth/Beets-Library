96937f6a748e085035722953377b0710e49b960c
Christopher Larson <kergoth@gmail.com>
2019-09-07 14:03:41 -0700

    Separate existing vs new field ignores for import_inspect
    There are fields that I want to see if they are being changed, but not
    if they're new, i.e. most release fields.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- Beets/plugins/import_inspect.py
+++ Beets/plugins/import_inspect.py
@@ -24,26 +24,24 @@ class ImportInspectPlugin(BeetsPlugin):
 
         self.config.add({
             'on_apply': True,
-            'field_whitelist': '',
-            'field_blacklist': '',
+            # Fields always ignored
+            'ignored': '',
+            # Fields ignored only when they're new (no previous value)
+            'ignored_new': '',
+            # Fields ignored only when they're existing (changed from previous value)
+            'ignored_existing': '',
         })
 
         self.album_fields = set(library.Album.item_keys)
         self.nonalbum_fields = library.Item._media_tag_fields.difference(self.album_fields)
-
-        self.whitelist = self.config['field_whitelist'].as_str_seq()
-        if self.whitelist:
-            self.album_fields = self.album_fields.intersection(self.whitelist)
-            self.nonalbum_fields = self.nonalbum_fields.intersection(self.whitelist)
-
-        self.blacklist = self.config['field_blacklist'].as_str_seq()
-        if self.blacklist:
-            self.album_fields = self.album_fields.difference(self.blacklist)
-            self.nonalbum_fields = self.nonalbum_fields.difference(self.blacklist)
-
         self.all_fields = list(sorted(self.album_fields | self.nonalbum_fields))
-        self.album_fields = list(sorted(self.album_fields))
-        self.nonalbum_fields = list(sorted(self.nonalbum_fields))
+
+        self.ignored = set(self.config['ignored'].as_str_seq())
+        self.album_fields = list(sorted(self.album_fields - self.ignored))
+        self.nonalbum_fields = list(sorted(self.nonalbum_fields - self.ignored))
+
+        self.ignored_new = set(self.config['ignored_new'].as_str_seq())
+        self.ignored_existing = set(self.config['ignored_existing'].as_str_seq())
 
         self.register_listener('before_choose_candidate',
                                self.before_choose_candidate_listener)
@@ -101,20 +99,34 @@ class ImportInspectPlugin(BeetsPlugin):
             newitems = list(newmapping.keys())
             values = dict((key, newitems[0][key]) for key in self.album_fields)
             album = library.Album(lib, **values)
+            compare_fields = self.get_fields(self.album_fields, oldvalues)
 
-            album_changes = ui.show_model_changes(album, oldalbum, self.album_fields)
+            album_changes = ui.show_model_changes(album, oldalbum, compare_fields)
             if album_changes:
                 changes = True
 
             new_by_info = {track_info: item for item, track_info in newmapping.items()}
             for item, track_info in match.mapping.items():
                 newitem = new_by_info[track_info]
-                item_changes = ui.show_model_changes(newitem, item, self.nonalbum_fields)
+                compare_fields = self.get_fields(self.nonalbum_fields, item)
+                item_changes = ui.show_model_changes(newitem, item, compare_fields)
                 if item_changes:
                     changes = True
         else:
             fakeitem = new_item(task.item)
             autotag.apply_item_metadata(fakeitem, match.info)
-            changes = ui.show_model_changes(fakeitem, task.item, self.all_fields)
+            compare_fields = self.get_fields(self.all_fields, task.item)
+            changes = ui.show_model_changes(fakeitem, task.item, compare_fields)
 
         return changes
+
+    def get_fields(self, keys, old):
+        compare_fields = []
+        for key in keys:
+            if old.get(key):
+                ignored = self.ignored_existing
+            else:
+                ignored = self.ignored_new
+            if key not in ignored:
+                compare_fields.append(key)
+        return compare_fields
