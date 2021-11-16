c448b2df6aa1348bec58734181f668984a3b25a4
Christopher Larson <kergoth@gmail.com>
2021-11-16 09:03:01 -0700

    kergoth: set advisory for tracks returned by spotifyexplicit
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- plugins/kergoth.py
+++ plugins/kergoth.py
@@ -5,7 +5,7 @@ from __future__ import division, absolute_import, print_function
 import beets
 from beets.library import DefaultTemplateFunctions
 from beets.plugins import BeetsPlugin, find_plugins
-from beets.ui import UserError
+from beets.ui import UserError, show_model_changes
 
 
 class KergothPlugin(BeetsPlugin):
@@ -17,6 +17,20 @@ class KergothPlugin(BeetsPlugin):
         super().__init__()
 
         self.register_listener("pluginload", self.loaded)
+        self.register_listener("spotify_explicit_track", self.spotify_explicit_track)
+
+    def spotify_explicit_track(self, lib, track, item):
+        if 'advisory' not in item:
+            item['advisory'] = 1
+            if show_model_changes(item):
+                item.store()
+
+        if item.album and item._cached_album:
+            album = item._cached_album
+            if 'albumadvisory' not in album:
+                album['albumadvisory'] = 1
+                if show_model_changes(album):
+                    album.store()
 
     def loaded(self):
         found_plugins = set()
